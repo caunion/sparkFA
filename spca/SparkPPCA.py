58375351d30ea854ec24bc67c6c4cf967827f22d
Daoyuan Jia <jiadaoyuan@gmail.com>
2016-12-03 00:49:10 -0500

    add java spca, modified the argument parsing

--- spca/SparkPPCA.py
+++ spca/SparkPPCA.py
@@ -1,7 +1,10 @@
 import logging
+from PCAUtils import PCAUtils
 import os.path as path
 import numpy as np
 from pyspark.mllib.linalg import Matrices, SparseVector, Vectors
+import pyspark
+
 
 class SparkPPCA:
     logger = logging.getLogger("SparkPPCA")
@@ -29,4 +32,49 @@ class SparkPPCA:
         :param computeProjectedMatrix:
         :return:
         """
-        pass
+
+        print ("Rows: {0}, Cols: {1}".format(nRows, nCols))
+
+        # The two PPCA variables that improve over each iteration: Principal component
+        # matrix (C) and random variance (ss), initialized randomly for the first iteration
+        ss = PCAUtils.randSS()
+        centralC = PCAUtils.randomMatrix(nCols, nPCs)
+
+        # 1. Mean job: This job calculates the mean and span of the columns of the input
+        # RDD<pyspark.mllib.linalg.Vector>
+        matrixAccumY = sc.accumulator(np.zeros([1, nCols]), VectorAccumulatorAbsParam())
+        internalSumY = np.zeros([1, nCols])
+
+        def func1(iter):
+            for yi in iter:
+                indices = yi.indices()
+                internalSumY[indices] += yi[indices]
+            matrixAccumY.add(internalSumY)
+        vectors.foreachPartition(func1)
+        # end mean job
+
+
+
+class MatrixAccumulatorParam(pyspark.accumulator.AccumulatorParam):
+
+    def addInPlace(self, mat1, mat2):
+        mat1 += mat2
+        return mat1
+
+    def zero(self, mat):
+        return mat
+
+    def addAccumulator(self, mat1, mat2):
+        return self.addInPlace(mat1, mat2)
+
+class VectorAccumulatorAbsParam(pyspark.accumulator.AccumulatorParam):
+    """
+        parameters should all be vector!
+    """
+    def addInPlace(self, vec1, vec2):
+        vec1 += vec2
+        return vec1
+    def zero(self, vec):
+        return vec
+    def addAccumulator(self, vec1, vec2):
+        return self.addInPlace(vec1, vec2)
